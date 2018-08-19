67a63bc8271fa8ac59e65a5bcd0854aeebc97536
Michelle Olivieri <michelleo72@gmail.com>
2018-08-19 19:53:08 -0400

    working on recipes/coming soon component

---
+++ client/src/components/Recipes.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Recipes = () => {
+  return (
+    <div className="recipes">
+      <h3>Coming Soon!</h3>
+      <p>Hosting a brunch at home? Soon, you can share recipes and hosting tips with fellow brunch lovers!
+      In the meantime, please enjoy a recipe from Giada De Laurentiis!</p>
+
+      <h4>Lemon-Ricotta Pancakes</h4>
+      <h5>Ingredients</h5>
+        <ul>
+          <li>2/3 cup all-purpose flour</li>
+          <li>1/3 cup polenta</li>
+          <li>1 teasppon baking powder</li>
+          <li>1/4 teaspoon kosher salt</li>
+          <li>1 cup buttermilk</li>
+          <li>1/2 cup part-skim ricotta cheese</li>
+          <li>2 teasponss greated lemon zest (2 lemons)</li>
+          <li>2 eggs</li>
+          <li>Grapeseed oil</li>
+          <li>Maple syrup, warmed, for serving</li>
+        </ul>
+      <h5>Directions</h>
+    </div>
+  )
+}
